c6c83a6e3482824646135e842d4ce7f74f5b7d58
sujung-lim <sujunglim94@gmail.com>
2023-08-06 20:44:58 +0900

    feature: 이벤트 핸들링 변경 #1

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const addBtn = document.querySelector('.btn-add');
+const newTaskForm = document.querySelector('.new-task-form');
 
-addBtn.addEventListener('click', () => {
-  const newTaskInput = document.querySelector('.new-task-title');
-  const newTaskInputText = newTaskInput.value;
+newTaskForm.addEventListener('submit', e => {
+  e.preventDefault();
+  /* submit 이벤트가 발생하면 폼이 기본적으로 페이지를 새로고침함
+   * 그래서 Add 버튼을 누르면 리스트가 생겼다가 바로 사라짐
+   * 이 문제를 방지하기 위해 preventDefault() 사용
+   */
+
+  const newTaskTitle = document.querySelector('.new-task-title').value;
+  const newTaskContent = document.querySelector('.new-task-content').value;
 
   // <li>
   const todoUl = document.querySelector('#ul-todo-list');
@@ -27,14 +33,33 @@ addBtn.addEventListener('click', () => {
   });
 
   // <p> 리스트 제목
-  const todoP = document.createElement('p');
-  todoP.innerText = newTaskInputText;
-  todoP.classList.add('todoTitle');
-  todoLi.appendChild(todoP);
+  const todoTitle = document.createElement('p');
+  todoTitle.innerText = newTaskTitle;
+  todoTitle.classList.add('todo-Title');
+  todoLi.appendChild(todoTitle);
 
+  // 리스트 제목 클릭하면 상세 내용 보여주기
+  let contentAdded = false;
+
+  /*******여기 해야함 */
+  todoTitle.addEventListener('click', () => {
+    if (!contentAdded) {
+      contentAdded = true;
+      showTodoContent();
+    }
+  });
+
+  // 상세 내용 보여주기 함수
+  function showTodoContent() {
+    const todoContent = document.createElement('p');
+    todoContent.innerText = newTaskContent;
+    todoContent.classList.add('todo-content');
+    todoUl.appendChild(todoContent);
+  }
   // <button> 수정 버튼
   const editBtn = document.createElement('button');
   editBtn.innerHTML = 'Edit';
+  editBtn.classList.add('edit-btn');
   todoLi.appendChild(editBtn);
 
   editBtn.addEventListener('click', () => {
@@ -61,6 +86,7 @@ addBtn.addEventListener('click', () => {
   // <button> 삭제 버튼
   const deleteBtn = document.createElement('button');
   deleteBtn.innerHTML = 'Delete';
+  deleteBtn.classList.add('delete-btn');
   todoLi.appendChild(deleteBtn);
 
   deleteBtn.addEventListener('click', () => {
@@ -68,5 +94,5 @@ addBtn.addEventListener('click', () => {
   });
 
   // 입력창 비우기
-  newTaskInput.value = '';
+  newTaskTitle.value = '';
 });
