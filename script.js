db90746de909db6adb47d6f1bac3e09f6360bb6c
sujung-lim <sujunglim94@gmail.com>
2023-08-07 23:22:04 +0900

    Modify: todolist -> 게시판 변경 작업 중, table 요소 추가

--- script.js
+++ script.js
@@ -1,98 +1,119 @@
-const newTaskForm = document.querySelector('.new-task-form');
+const newMessageForm = document.querySelector('.new-message-form');
 
-newTaskForm.addEventListener('submit', e => {
+newMessageForm.addEventListener('submit', e => {
   e.preventDefault();
   /* submit 이벤트가 발생하면 폼이 기본적으로 페이지를 새로고침함
    * 그래서 Add 버튼을 누르면 리스트가 생겼다가 바로 사라짐
    * 이 문제를 방지하기 위해 preventDefault() 사용
    */
+  // <table>
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  table.appendChild(tableHead);
 
-  const newTaskTitle = document.querySelector('.new-task-title').value;
-  const newTaskContent = document.querySelector('.new-task-content').value;
+  const headerRow = document.createElement('tr');
+  tableHead.appendChild(headerRow);
 
-  // <li>
-  const todoUl = document.querySelector('#ul-todo-list');
-  const todoLi = document.createElement('li');
-  todoLi.id = 'li-todo-list';
-  todoUl.appendChild(todoLi);
-
-  // <input type="checkbox"> 체크박스
-  const todoCheck = document.createElement('input');
-  todoCheck.type = 'checkbox';
-  todoCheck.classList.add('checkbox');
-  todoLi.appendChild(todoCheck);
-
-  // checkbox가 체크되면 Completed 리스트로 옮기기
-  todoCheck.addEventListener('change', function () {
-    const completedUl = document.querySelector('#ul-completed-list');
-    if (this.checked) {
-      completedUl.appendChild(todoLi);
-    } else if (!this.checked) {
-      todoUl.appendChild(todoLi);
-    }
+  const headers = ['작성자', '제목', '작성날짜', '조회수'];
+  headers.forEach(header => {
+    const headerCell = document.createElement('th');
+    headerCell.innerText = header;
+    headerRow.appendChild(headerCell);
   });
 
+  // <tbody>
+  const tableMessageList = document.createElement('tbody');
+  tableMessageList.setAttribute('id', 'table-message-list');
+  table.appendChild(tableMessageList);
+
+  const newMessageWriter = document.querySelector('.new-message-writer').value;
+  const newMessageTitle = document.querySelector('.new-message-title').value;
+  const newMessageContent = document.querySelector(
+    '.new-message-content'
+  ).value;
+
+  // <li>
+  const messageUl = document.querySelector('#ul-message-list');
+  const messageLi = document.createElement('li');
+  messageLi.id = 'li-message-list';
+  messageUl.appendChild(messageLi);
+
   // <p> 리스트 제목
-  const todoTitle = document.createElement('p');
-  todoTitle.innerText = newTaskTitle;
-  todoTitle.classList.add('todo-Title');
-  todoLi.appendChild(todoTitle);
+  const messageTitle = document.createElement('p');
+  messageTitle.innerText = newMessageTitle;
+  messageTitle.classList.add('message-Title');
+  messageLi.appendChild(messageTitle);
 
   // 리스트 제목 클릭하면 상세 내용 보여주기
   let contentAdded = false;
 
-  /*******여기 해야함 */
-  todoTitle.addEventListener('click', () => {
+  messageTitle.addEventListener('click', () => {
     if (!contentAdded) {
       contentAdded = true;
-      showTodoContent();
+      showMessageContent();
+    } else {
+      contentAdded = false;
+      hideMessageContent();
     }
   });
 
   // 상세 내용 보여주기 함수
-  function showTodoContent() {
-    const todoContent = document.createElement('p');
-    todoContent.innerText = newTaskContent;
-    todoContent.classList.add('todo-content');
-    todoUl.appendChild(todoContent);
+  function showMessageContent() {
+    const messageContent = document.createElement('p');
+    messageContent.innerText = newMessageContent;
+    messageContent.classList.add('message-content');
+    messageUl.appendChild(messageContent);
   }
-  // <button> 수정 버튼
-  const editBtn = document.createElement('button');
-  editBtn.innerHTML = 'Edit';
-  editBtn.classList.add('edit-btn');
-  todoLi.appendChild(editBtn);
 
-  editBtn.addEventListener('click', () => {
-    const todoTitle = todoLi.querySelector('.todoTitle');
-    if (todoTitle.tagName.toLowerCase() === 'p') {
-      const input = document.createElement('input');
-      input.value = todoTitle.textContent;
-
-      todoLi.replaceChild(input, todoTitle);
-      input.focus();
-
-      input.addEventListener('keydown', e => {
-        if (e.key === 'Enter') {
-          const newTitle = input.value;
-          const newP = document.createElement('p');
-          newP.classList.add('todoTitle');
-          newP.innerText = newTitle;
-          todoLi.replaceChild(newP, input);
-        }
-      });
+  // 상세 내용 숨기기 함수
+  function hideMessageContent() {
+    const messageContent = document.querySelector('.message-content');
+    if (messageContent) {
+      messageContent.remove();
     }
-  });
+  }
 
-  // <button> 삭제 버튼
-  const deleteBtn = document.createElement('button');
-  deleteBtn.innerHTML = 'Delete';
-  deleteBtn.classList.add('delete-btn');
-  todoLi.appendChild(deleteBtn);
+  // // <button> 수정 버튼
+  // const editBtn = document.createElement('button');
+  // editBtn.innerHTML = 'Edit';
+  // editBtn.classList.add('edit-btn');
+  // messageLi.appendChild(editBtn);
 
-  deleteBtn.addEventListener('click', () => {
-    todoLi.remove();
-  });
+  // editBtn.addEventListener('click', () => {
+  //   const messageTitle = messageLi.querySelector('.messageTitle');
+  //   if (messageTitle.tagName.toLowerCase() === 'p') {
+  //     const input = document.createElement('input');
+  //     input.value = messageTitle.textContent;
+
+  //     messageLi.replaceChild(input, messageTitle);
+  //     input.focus();
+
+  //     input.addEventListener('keydown', e => {
+  //       if (e.key === 'Enter') {
+  //         const newTitle = input.value;
+  //         const newP = document.createElement('p');
+  //         newP.classList.add('messageTitle');
+  //         newP.innerText = newTitle;
+  //         messageLi.replaceChild(newP, input);
+  //       }
+  //     });
+  //   }
+  // });
+
+  // // <button> 삭제 버튼
+  // const deleteBtn = document.createElement('button');
+  // deleteBtn.innerHTML = 'Delete';
+  // deleteBtn.classList.add('delete-btn');
+  // messageLi.appendChild(deleteBtn);
+
+  // deleteBtn.addEventListener('click', () => {
+  //   const messageContent = document.querySelector('.message-content');
+  //   messageLi.remove();
+  //   if (messageContent) {
+  //     messageContent.remove();
+  //   }
+  // });
 
   // 입력창 비우기
-  newTaskTitle.value = '';
+  newMessageTitle.value = '';
 });
